221d861da9e489b92be294e6948882fc4db6ad06
Aanya12345 <aanyapad@gmail.com>
2024-09-26 18:43:57 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function schools(){
+    document.getElementById("div1").style = `display: block`;
+}
+function close1(){
+    document.getElementById("div1").style = `display: none`;
+}
+function displaySearchBar(){
+    document.getElementById("ip").style="display:block;";
+    document.getElementById("searchBarIcon").style= `position:relative;
+    top:22px; padding-left:5px;`;
+}
+function blockbar(){
+    document.getElementById("ip").style="display:none;";
+}
